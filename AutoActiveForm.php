b9532d3226d03e5bae53e29f927d48169d8405fe
Gianpaolo Scrigna <letsjump@gmail.com>
2014-11-02 13:08:47 +0100

    Replaced $guestName with Yii::app()->user->guestName Improved documentation

--- AutoActiveForm.php
+++ AutoActiveForm.php
@@ -1,49 +1,98 @@
 <?php
 
 /**
- * Yii-AutoActiveForm
+ * AutoActiveForm class file
  * A faster way to setup form fields in Yii Framework 1.1
  *
  * @author  Gianpaolo Scrigna <letsjump@gmail.com>
  * @link    http://github.com/letsjump/Yii.AutoActiveForm
- * @license http://www.opensource.org/licenses/bsd-license.php
- * @version $Id: AutoActiveForm.php 2 2014-11-01 16:13 letsjump $
+ * @copyright Copyright &copy; 2008-2011 Yii Software LLC, 2013 Gianpaolo Scrigna
+ * @license http://www.yiiframework.com/license/
+ *
+ * In a web full of complex html form templates, this extension provides
+ * a faster way to draw html form fields in Yii 1.1.
+ * It also has an Access control system to give
+ * read / write access to every form component
+ * according Yii RBAC access rules.
  */
 
 class AutoActiveForm extends CActiveForm
 {
-
-    public $labelHtmlOptions = Array();
-
-    public $errorHtmlOptions = Array();
-
+	/**
+	 * @var string $viewPath Path alias
+	 * to autoActiveForm view folder
+	 */
 	public $viewPath = 'ext.autoActiveForm.views';
 
+	/**
+	 * @var string $viewFile the name of the default view
+	 */
     public $viewFile = 'default';
 
+	/**
+	 * @var bool $accessControl Enable or disable access control for each field
+	 */
     public $accessControl = false;
 
-	public $guestRoleName = 'guest'; // sistemare in base a CWebUser.php user->GuestName
+	/**
+	 * @var string $allowAction The action to perform when access control is disabled
+	 *                          Usually the action that draw field with label and error
+	 */
+	public $allowAction = 'write';
 
-    public $allowAction = 'write';
-
+	/**
+	 * @var bool $showLabels Main label generator switch
+	 *                       Usually set to false when use placeholder
+	 */
 	public $showLabels = true;
 
+	/**
+	 * @var bool $labelToPlaceholder create a placeholder property
+	 *                               and put label text into it
+	 */
 	public $labelToPlaceholder = false;
 
-    public $extractFromHtmlOptions = Array('errorHtmlOptions', 'labelHtmlOptions', 'jsOptions', 'roValue', 'viewFile', 'itemTemplate');
+	/**
+	 * @var array $extractFromHtmlOptions
+	 */
+	public $extractFromHtmlOptions = Array(
+		'errorHtmlOptions',
+		'labelHtmlOptions',
+		'jsOptions',
+		'roValue',
+		'viewFile',
+		'itemTemplate'
+	);
 
-    protected $params;
+	/**
+	 * @var object $params used to build field property
+	 */
+	protected $params;
 
-    protected $_fieldsPermissions;
+	/**
+	 * @var object $_fieldPermission Object of permission read from
+	 *                               method getAccessRules()
+	 */
+	protected $_fieldsPermissions;
 
-    public $roTemplate = "<span class='read'>{value}&nbsp;</span>";
-	
+	/**
+	 * @var string $roTemplate Template for field in readonly mode
+	 */
+	public $roTemplate = "<span class='read'>{value}&nbsp;</span>";
+
+	/**
+	 * Initializes the widget.
+	 * This renders the form open tag.
+	 */
 	public function init()
     {
         parent::init();
     }
 
+	/**
+	 * Runs the widget.
+	 * This registers the necessary javascript code and renders the form close tag.
+	 */
     public function run()
     {
         parent::run();
@@ -351,12 +400,13 @@ class AutoActiveForm extends CActiveForm
     /**
      * Build a standard CHtml field with label and error,
      * if pass required access control
-     * @param array $method Class::Method that build the field
-     * @param obj $model Model that render with the form
-     * @param string $attribute of the model
+     * @param array $method Class::Method needed to build the field
+     * @param CActiveRecord $model Form's related model
+     * @param string $attribute Form's model related attribute
      * @param array $data of select, multiselect ecc... fields
      * @param array $htmlOptions html attributes that render with the field
-     * @return html the field formatted with label & error
+	 *
+     * @return string CHtml field formatted with label & error
      */
     protected function buildField($method, $model, $attribute, $data, $htmlOptions)
     {
@@ -369,24 +419,35 @@ class AutoActiveForm extends CActiveForm
 
         $this->params->args[] = $model;
         $this->params->args[] = $attribute;
-        if (!empty($data)) {
+        if (!empty($data))
+		{
             $this->params->args[] = $data;
         }
         $this->params->args[] = $this->params->htmlOptions;
 
         // Access control
-        if ($this->accessControl === false || Yii::app()->user->checkAccess('admin')) {
+        if ($this->accessControl === false
+			|| Yii::app()->user->checkAccess('admin')
+		)
+		{
             return call_user_func ('self::'.$this->allowAction);
-        } else {
+        }
+		else
+		{
             $design = $this->getFieldsPermissions($model);
-
-            if(isset($design->$attribute) && method_exists($this, $design->$attribute)) {
+            if(isset($design->$attribute) && method_exists($this, $design->$attribute))
+			{
                 return call_user_func ('self::'.$design->$attribute);
 			}
         }
     }
 
-    protected function extractOptions($htmlOptions)
+	/**
+	 * Extract parameters specified with property $extractFromHtmlOptions
+	 * passed by CActiveForm property  $htmlOptions array
+	 * @param $htmlOptions
+	 */
+	protected function extractOptions($htmlOptions)
     {
         foreach($this->extractFromHtmlOptions as $option)
         {
@@ -396,7 +457,7 @@ class AutoActiveForm extends CActiveForm
                 {
                     $this->params->$option = CMap::mergeArray($this->$option, $htmlOptions[$option]);
                 }
-                    else
+                else
                 {
                     $this->params->$option = $htmlOptions[$option];
                 }
@@ -414,10 +475,20 @@ class AutoActiveForm extends CActiveForm
         $this->params->htmlOptions = $htmlOptions;
     }
 
+	/**
+	 * Create a CHtml label and / or add a placeholder property to its field
+	 * If property $labelToPlaceholder is TRUE, add placeholder property
+	 * to its related field
+	 * If property $showLabels is FALSE it returns an empty string
+	 *
+	 * @return string CHtml::labelEx Label if $showLabels is TRUE
+	 *                else returns an empty string
+	 */
 	protected function createLabel() {
 		$attribute = $this->params->attribute;
 		$model     = $this->params->model;
-		if ($this->labelToPlaceholder === true && !isset($this->params->htmlOptions['placeholder']))
+		if ($this->labelToPlaceholder === true
+			&& !isset($this->params->htmlOptions['placeholder']))
 		{
 			$attributeLabels = $model->attributeLabels();
 			if(isset($attributeLabels[$attribute]))
@@ -429,32 +500,54 @@ class AutoActiveForm extends CActiveForm
 		{
 			return $this->labelEx($model, $attribute, $this->params->labelHtmlOptions);
 		}
+		else
+		{
+			return "";
+		}
 	}
 
-    protected function write()
+	/**
+	 * Render the field with write permissions
+	 * based on $viewFile template
+	 *
+	 * @return string CHTML field formatted with label and error
+	 */
+	protected function write()
     {
 		$viewFile = (!empty($this->params->viewFile)) ? $this->params->viewFile : $this->viewFile;
         $data = new stdClass();
         $data->label = $this->createLabel();
-        if($this->params->data != null) {
+        if($this->params->data != null)
+		{
             $data->field = call_user_func($this->params->method, $this->params->model, $this->params->attribute, $this->params->data, $this->params->htmlOptions);
-        } else {
+        }
+		else
+		{
             $data->field = call_user_func($this->params->method, $this->params->model, $this->params->attribute, $this->params->htmlOptions);
         }
         $data->error = $this->error($this->params->model,$this->params->attribute, $this->params->errorHtmlOptions);
+
         return $this->render($this->viewPath . '.' . $viewFile, $data, TRUE);
     }
 
-    protected function read()
+	/**
+	 * Render the field value with read permissions
+	 * based on $roTemplate template
+	 *
+	 * @return string CHTML string formatted with label
+	 */
+	protected function read()
     {
         $data = new stdClass();
         $attribute = $this->params->attribute;
 		$model = $this->params->model;
 		$roValue = '';
         $data->label = $this->createLabel();
-		if (!empty($this->params->roValue)) {
+		if (!empty($this->params->roValue))
+		{
 			$roValue = $this->params->roValue;
-		} elseif (!empty($model->$attribute)) {
+		} elseif (!empty($model->$attribute))
+		{
 			$roValue = $model->$attribute;
 		}
         $data->field = str_replace('{value}', $roValue, $this->roTemplate);
@@ -462,9 +555,16 @@ class AutoActiveForm extends CActiveForm
         return $this->render($this->viewPath . '.' . $this->viewFile, $data, TRUE);
     }
 
-    protected function getFieldsPermissions($model)
+	/**
+	 * Build an object with each field's permissions
+	 * based on user's Yii RBAC Access Rules
+	 * depending on a scenario if specified...
+	 * @param CActiveRecord $model of the form
+	 *
+	 * @return object of field's permission
+	 */
+	protected function getFieldsPermissions($model)
     {
-//		l'errore sta qui da qualche parte!!!!!!
         if(empty($this->_fieldsPermissions) && !empty($model->accessRules))
         {
             $scenario = $model->getScenario();
@@ -472,13 +572,12 @@ class AutoActiveForm extends CActiveForm
             foreach($model->accessRules as $rule=>$opeArray)
             {
                 if(
-				($rule != $this->guestRoleName && Yii::app()->user->checkAccess($rule))
-					|| ($rule == $this->guestRoleName && Yii::app()->user->isGuest)
+					($rule != Yii::app()->user->guestName && Yii::app()->user->checkAccess($rule))
+					|| ($rule == Yii::app()->user->guestName && Yii::app()->user->isGuest)
 				)
                 {
                     foreach($opeArray as $ope)
                     {
-                        //$fields = explode(',', str_replace(' ', '', $ope[0]));
 						$fields = explode(',', preg_replace('/\s/', '', $ope[0]));
                         if(!isset($ope['on']))
                         {
@@ -491,7 +590,6 @@ class AutoActiveForm extends CActiveForm
                         {
                             foreach($fields as $field)
                             {
-
 								$permissions->$field = $ope[1];
                             }
                         }
@@ -503,5 +601,3 @@ class AutoActiveForm extends CActiveForm
         return $this->_fieldsPermissions;
     }
 }
-
-?>
